44ef73e90cb298a124aaafcfbd281a339b255d32
wisemuji <wisemuji@gmail.com>
2019-03-26 09:57:52 +0900

    connecting database

--- flaskr/falskr.py
+++ flaskr/falskr.py
@@ -15,5 +15,8 @@ app.config.update(dict(
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-
-
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
