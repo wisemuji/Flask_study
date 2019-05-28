6ce129dfd32764d3953dea63888e166e2efeb9f0
wisemuji <wisemuji@gmail.com>
2019-05-29 00:38:17 +0900

    How to set and get cookies

---
+++ ex/cookies.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect, make_response
+
+app = Flask(__name__)
+
+@app.route("/example/cookie")
+def example_cookie():
+    print(request.cookies)
+    return ""
+
+@app.route("/example/cookie_set")
+def example_cookie_set():
+    redirect_to_cookie = redirect("/example/cookie")
+    response = make_response(redirect_to_cookie)
+    response.set_cookie('Cookie Register', value='Example Cookie')
+    return response
+
+app.run()
+
+
+
