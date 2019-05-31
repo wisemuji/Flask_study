fdc25690bed493ddf5ae34195280cd4c62d8830d
wisemuji <wisemuji@gmail.com>
2019-06-01 04:18:56 +0900

    Using SQLalchemy with 'flask-sqlalchemy

---
+++ flask_sqlalchemy_exam2/flask_sqlalchemy/flask_sqlalchemy.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+pypostgresql://sqlalchemy:sqlalchemy@localhost/sqlalchemy"
+db = SQLAlchemy(app)
+
+import models
---
+++ flask_sqlalchemy_exam2/flask_sqlalchemy/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name,)
